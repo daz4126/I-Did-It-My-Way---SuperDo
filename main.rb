f7d11ad1802e81c77d0e84179a2a3f303ada7af6
Darren Jones <daz@ironman.(none)>
2010-03-29 18:06:20 +0100

    List all and edit pages added

--- main.rb
+++ main.rb
@@ -12,11 +12,52 @@ class Task
   property :completed_at, DateTime
 end
 
+# list all tasks
+get '/tasks' do
+  @tasks = Task.all
+  erb :index
+end
+
+# new task
+get '/task/new' do
+  erb :new
+end
+
+# create new task   
+post '/task/create' do
+  task = Task.new(:name => params[:name])
+  if task.save
+    status 201
+    redirect '/task/'+task.id.to_s  
+  else
+    status 412
+    redirect '/tasks'   
+  end
+end
+
+# edit task
+get '/task/:id/edit' do
+  @task = Task.get(params[:id])
+  erb :edit
+end
+
+# update task
+put '/task/:id' do
+  task = Task.get(params[:id])
+  task.completed_at = params[:completed] ?  Time.now : nil
+  if @task.save
+    status 201
+    redirect "/task/"+task.id
+  else
+    status 412
+    redirect '/tasks'   
+  end
+end
+
 # View a task
 get '/task/:id' do
   @task = Task.get(params[:id])
   erb :task
 end
-
 
 DataMapper.auto_upgrade!
